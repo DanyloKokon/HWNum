a480fa704a3c63f72015a38030d2ad1791f66d06
DanyloKokon <149526893+DanyloKokon@users.noreply.github.com>
2024-11-04 18:32:54 +0100

    4wwsd

---
+++ JS/script.js
@@ -0,0 +1,58 @@
+let celsia = prompt("Enter the Celsia");
+let faranhait = celsia * 9 / 5 + 32;
+console.log(faranhait);
+
+
+let daysPerMonth = 31;
+let hoursPerMonth = daysPerMonth * 24;
+let minPerMonth = hoursPerMonth * 60;
+console.log("Hours per month: " + hoursPerMonth );
+console.log("Hours per month: " + minPerMonth );
+
+
+let healt = 100;
+let energi = 50;
+healt -= 30;
+energi -= 27;
+console.log(healt);
+console.log(energi);
+
+
+let caseIn = 227;
+let caseIs = 227;
+caseIs = caseIs - (caseIn * (10/100));
+console.log(caseIs);
+//Я не зрозумів я я маю за допомогою тільки одного оператора множення зробити це завдання
+ 
+
+let flyDot = parseFloat(5.031);
+flyDot = Math.floor(flyDot);
+console.log(flyDot);
+
+
+let pfTask = "44.99hsjj";
+pfTask = parseFloat(pfTask)
+console.log(pfTask)
+
+
+
+let piTask = "88983idbudsj";
+piTask = parseInt(piTask);
+console.log(piTask)
+
+
+let root = 625;
+root = Math.sqrt(root);
+console.log(root);
+
+
+let tsTask;
+let myString;
+
+tsTask = parseInt(23.08)
+console.log(tsTask);
+
+myString = tsTask.toString(23.98);
+console.log(myString)
+
+
